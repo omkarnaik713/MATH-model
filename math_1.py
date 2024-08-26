611e7cd311d9b2793e9f518e316e6cdd7ae11299
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2024-08-26 19:26:16 -0400

    Updated code

--- math_1.py
+++ math_1.py
@@ -59,56 +59,111 @@ if __name__ == '__main__' :
     converted_data = convert_dataset(question,answer)
     #tf_dataset = tf_math_datasets()
     
-    base_model = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+    base_model = 'NousResearch/Llama-2-7b-chat-hf'
     
-    llama_tokenizer = AutoTokenizer.from_pretrained(base_model,trust_remote_code = True )
-    llama_tokenizer.pad_token = llama_tokenizer.eos_token
-    llama_tokenizer.padding_side = 'right'
+    lora_r = 64
+    lora_alpha = 16
+    lora_dropout = 0.1
     
+    use_4bit = True
+    bnb_4bit_compute_dtype = 'float16'
+    bnb_4bit_quant_type = "nf4"
+    use_nested_quant = False
     
-    quant_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=False)
+    output_dir = "./results"
     
-    model = AutoModelForCausalLM.from_pretrained(base_model,
-                                                quantization_config = quant_config,
-                                                device_map = {'':0})
-    model.config.use_cache = False 
+    num_train_epochs = 1
+    
+    fp16 = False 
+    bf16 = False 
+    
+    per_device_train_batch_size = 4
+    per_device_eval_batch_size = 4
+    
+    gradient_accumulation_steps = 1
+    gradient_checkpointing = True 
+    
+    max_grad_norm = 0.3
+    learning_rate = 2e-4
+    weight_decay = 0.001
+    optim = "paged_adamw_32bit"
+    lr_scheduler_type = 'cosine'
+    max_steps = -1
+    warmup_ratio = 0.03
+    group_by_length = True
+    
+    logging_steps = 25
+    
+    max_seq_length = None 
+    packing = False
+    
+    ## loading the tokenizer 
+    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+    
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=use_4bit,
+        bnb_4bit_quant_type=bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=use_nested_quant
+    )
+    
+    ## load the base model 
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        quantization_config = bnb_config,
+        device_map = 'auto'
+    )
+    model.config.use_cache = False
     model.config.pretraining_tp = 1
     
-    peft_parameters = LoraConfig(lora_alpha= 16,
-                            lora_dropout= 0.1,
-                            r = 8,
-                            bias = 'none',
-                            task_type='CASUAL_LM')
+    tokenizer = AutoTokenizer.from_pretrained(
+        base_model,
+        trust_remote_code = True
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = 'right'
     
-    train_params = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=2,
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=1,
-    optim="paged_adamw_32bit",
-    save_steps=25,
-    logging_steps=25,
-    learning_rate=2e-4,
-    weight_decay=0.001,
-    fp16=False,
-    bf16=False,
-    max_grad_norm=0.3,
-    max_steps=-1,
-    warmup_ratio=0.03,
-    group_by_length=True,
-    lr_scheduler_type="constant",
-    report_to="tensorboard")
+    ## loading LoRA Configuration 
     
-    fine_tuning = SFTTrainer(
-    model=model,
-    train_dataset=dataset,
-    peft_config=peft_parameters,
-    dataset_text_field="text",
-    tokenizer=llama_tokenizer,
-    args=train_params)
+    peft_config = LoraConfig(
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        r = lora_r,
+        bias = 'none',
+        task_type='CAUSAL_LM',
+    )
     
-    fine_tuning.train()
+    ## setting training parameters 
+    training_arguments = TrainingArguments(
+        output_dir = output_dir,
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim= optim,
+        save_steps = 25,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        fp16=fp16,
+        bf16=bf16,
+        max_grad_norm=max_grad_norm,
+        max_steps=max_steps,
+        warmup_ratio=warmup_ratio,
+        group_by_length=group_by_length,
+        lr_scheduler_type=lr_scheduler_type,
+        report_to='tensorboard'
+    )
+    
+    trainer = SFTTrainer(
+        model = model,
+        train_dataset = converted_data,
+        peft_config=peft_config,
+        dataset_text_field='text',
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args = training_arguments,
+        packing = packing,
+    )
+    
+    trainer.train()
